5546c2352d0cab3abe75cbb585d4f622a3739605
Абдусаламов Рустам <144446665+Rustam3383@users.noreply.github.com>
2025-06-27 21:13:50 +0300

    Загрузка данных для алгоритма из файла

---
+++ input.py
@@ -0,0 +1,31 @@
+import random
+
+# генерация n случайных точек. 
+def generate_random_points(n):
+    points = []
+    for i in range(n):
+        point = (random.uniform(0, 10), random.uniform(0, 10))
+        points.append(point)
+    return points
+
+# загрузка M окружностей и n точек из файла
+def load_data(filename, n):
+    points = []
+    M = 0 # число окружностей
+    try:
+        with open(filename, 'r') as file:
+            M = int(file.readline())
+            for i in range(n):
+                line = file.readline()
+                if not line:
+                    break
+                point = tuple(map(float, line.split()))
+                points.append(point)
+    except FileNotFoundError:
+        print(f"Файл {filename} не найден. Точки сгенерируются случайно.")
+        print("Введите число окружностей: ", end='')
+        M = int(input())
+        points = generate_random_points(n)
+    return M, points
+
+print(load_data("k", 5))
